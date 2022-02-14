2a485845da55cbb37a7be72ba71810ac20c1a1c0
KaeroDot <KaeroDot@contact.me.on.git.hub>
2022-02-14 10:54:31 +0100

    chng: calibration of data piece moved to standalone m file

--- alg sources/qpsw_process.m
+++ alg sources/qpsw_process.m
@@ -6,66 +6,13 @@
 % Result are waveforms without digitizer errors.
 
 
-function res = qpsw_process(sigconfig, D, S, M, Uref, Spjvs, alg);
+function [y, yc, res] = qpsw_process(sigconfig, D, S, M, Uref, Spjvs, alg);
     % initialize %<<<1
     DEBUG = 0;
 
-    % split multiplexed data %<<<1
+    % split multiplexed data and calibrate %<<<1
     yc = qpsw_demultiplex_split(D, S, M);
-
-    % calibration curves for whole sampled data %<<<2
-    % get calibration data for every quantum data piece %<<<2
-    empty_ycal.coefs = [];
-    empty_ycal.exponents = [];
-    empty_ycal.func = [];
-    empty_ycal.model = [];
-    empty_ycal.yhat = [];
-    for i = 1:rows(yc)
-            for j = 1:columns(yc)
-                    % check if quantum measurement:
-                    if M(i, j) < 0
-                            % do calibration
-                            % cut Spjvs and subtract to get indexes of the
-                            % cutted yc{i,j}
-                            idx = find(Spjvs >= S(j) & Spjvs < S(j+1));
-                            tmpSpjvs = Spjvs(idx) - S(j) + 1;
-                            tmpUref = Uref(idx);
-                            if tmpSpjvs(1) ~= 1
-                                tmpSpjvs = [1 tmpSpjvs];
-                                % add one Uref before, because first switch was
-                                % not at position 1
-                                tmpUref = [Uref(idx(1)-1) tmpUref];
-                            end
-                            if tmpSpjvs(end) ~= size(yc{i,j},2) + 1
-                                tmpSpjvs = [tmpSpjvs size(yc{i,j},2) + 1];
-                                % no need to add Uref, it is already there
-                            end
-                            ycal(i,j) = adc_pjvs_calibration(yc{i,j}, tmpSpjvs, tmpUref, sigconfig.Rs, sigconfig.Re);
-                    else
-                            ycal(i,j) = empty_ycal;
-                    end % if M(i, j) < 0
-            end % for j = 1:columns(yc)
-    end % for i = 1:rows(yc)
-
-    % set calibration values for all sampled data %<<<1
-    for i = 1:rows(ycal)
-            lastcal = empty_ycal;
-            firstcalfound = 0;
-            for j = 1:columns(ycal)
-                    if isempty(ycal(i, j).coefs)
-                            ycal(i, j) = lastcal;
-                    else
-                            lastcal = ycal(i, j);
-                            if firstcalfound == 0
-                                    % copy calibrations to previous elements:
-                                    for k = 1:j-1
-                                            ycal(i, k) = ycal(i,j);
-                                    end % for k
-                                    firstcalfound = 1;
-                            end % firstcalfound = 0
-                    end % isempty(ycal(i,j))
-            end % for j = 1:columns(yc)
-    end % for i = 1:rows(yc)
+    ycal = calibrate_data_pieces(yc, M, S, Uref, Spjvs, sigconfig.Rs, sigconfig.Re);
 
     % debug plot gains through time %<<<1
     if DEBUG
--- alg sources/qpsw_test.m
+++ alg sources/qpsw_test.m
@@ -1,6 +1,6 @@
 % debug: generate input data %<<<1
 clear all, close all
-sysconfig = 1;
+sysconfig = 3;
 sigconfig.f = 50;
 sigconfig.A = [10 7 5 3 1 0.5];
 sigconfig.ph = [0 0 0 0 0 0];
