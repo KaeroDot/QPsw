a7bdd079829fb8e30bed5515f77a2cd6b8649e1e
KaeroDot <KaeroDot@contact.me.on.git.hub>
2022-06-10 09:48:13 +0200

    add: new methods for identification of PJVS phase, much faster plotting

--- alg sources/pjvs_ident_segments.m
+++ alg sources/pjvs_ident_segments.m
@@ -24,6 +24,8 @@
 % Spjvs - indexes of segments (samples just after PJVS step change happened).
 %
 function Spjvs = pjvs_ident_segments(y, MRs, MRe, max_adc_noise, segmentlen, dbg)
+   pjvs_phase_method = 2;  %XXX move to sigconfig!
+
     % initialize %<<<1
 
     % check inputs %<<<1
@@ -53,34 +55,16 @@ function Spjvs = pjvs_ident_segments(y, MRs, MRe, max_adc_noise, segmentlen, dbg
     y2 = y;
     y2(1 : MRs) = y2(MRs);                  % works even for MR = 0;
     y2(end - MRe : end) = y2(end - MRe);    % works even for MRe = 0;
-    % find step changes:
-    % derivation shows out voltage changes:
-    y3 = abs(diff(y2));
-    % mask out small voltage changes caused by ADC noise:
-    y3(y3 < max_adc_noise) = 0;
-    % get indexes of the found segments, i.e. starts of PJVS steps. These
-    % indexes can still contain false positives or negatives:
-    ids = find(y3);
 
-    % find PJVS phase %<<<1
-    % this method is fast, but relies on the largest PJVS step.
-    % get index of biggest derivation (biggest signal step);
-    [~,maxid] = max(y3);
-    maxid = maxid(1);
-    % remainder of division is offset - number of samples at beginning before
-    % first PJVS step happen:
-    phase_offset = rem(maxid, segmentlen);
-    % indexes of PJVS steps:
-    Spjvs = [segmentlen:segmentlen:numel(y)] + phase_offset;
-    % ensure integers:
-    Spjvs = round(Spjvs);
-    % ensure proper values of indexes:
-    if Spjvs(1) < 1
-        Spjvs(1) = [];
-    end
-    if Spjvs(end) > numel(y) + 1
-        Spjvs(end) = [];
-    end
+    % call one of phase identification methods:
+    switch pjvs_phase_method
+        case 1
+            Spjvs = highest_peak(y, segmentlen);
+        case 2
+            Spjvs = split_and_highest_peak(y, segmentlen);
+        otherwise
+            error('pjvs_ident_segments: uknown method for identification of PJVS phase')
+    end % switch
 
     % ensure start and ends of record as PJVS segments
     if Spjvs(1) ~= 1
@@ -102,14 +86,12 @@ function Spjvs = pjvs_ident_segments(y, MRs, MRe, max_adc_noise, segmentlen, dbg
             hold on
             plot( y(1:plotmax),'-x');
             plot(y2(1:plotmax),'-');
-            plot(y3(1:plotmax),'-x');
-            ylims = ylim;
+            % plot(y3(1:plotmax),'-x'); %XXX
             plot([0, numel(plotmax)], [max_adc_noise max_adc_noise])
-            plot(ids(ids<plotmax), y3(ids(ids<plotmax)), 'ok')
-            plotx = [Spjvs(Spjvs<plotmax); Spjvs(Spjvs<plotmax)];
-            ploty = repmat(ylims', 1, numel(Spjvs(Spjvs<plotmax)));
-            plot(plotx, ploty, '--c');
-            legend('samples', 'samples with masked start/end', 'abs(diff(y))', 'max_adc_noise', 'ids - first identified segment starts', 'final segment starts')
+            tmpSpjvs = Spjvs(Spjvs < plotmax);
+            plot(tmpSpjvs, y(tmpSpjvs), 'o', 'linewidth', 2);
+            % legend('samples', 'samples with masked start/end', 'abs(diff(y))', 'max_adc_noise', 'ids - first identified segment starts', 'final segment starts')
+            legend('samples', 'samples with masked start/end', 'max_adc_noise', 'identified start of segment')
             title(sprintf('PJVS section with PJVS phase identification\nfirst 10 segments after MRs'))
             hold off
             fn = fullfile(dbg.plotpath, [ssec 'pjvs_ident_segments_10']);
@@ -121,18 +103,15 @@ function Spjvs = pjvs_ident_segments(y, MRs, MRe, max_adc_noise, segmentlen, dbg
         if dbg.pjvs_ident_segments_all
             % plot samples and ids - this plot takes VERY long time to draw!
             disp('plotting debug plot for identify_pjvs_segments_all. This will take a long time...')
-            figure('visible',dbg.showplots)
+            % figure('visible',dbg.showplots)
+            figure
             hold on
             plot(y,'-x');
             plot(y2,'-');
-            plot(y3,'-x');
-            ylims = ylim;
             plot([0, numel(y)], [max_adc_noise max_adc_noise])
-            plot(ids, y3(ids), 'ok')
-            plotx = [Spjvs; Spjvs];
-            ploty = repmat(ylims', 1, numel(Spjvs));
-            plot(plotx, ploty, '--c');
-            legend('samples', 'samples with masked start/end', 'abs(diff(y))', 'max_adc_noise', 'ids - first identified segment starts', 'final segment starts')
+            plot(Spjvs(1:end-1), y(Spjvs(1:end-1)), 'o', 'linewidth', 2);
+            legend('samples', 'samples with masked start/end', 'max_adc_noise', 'identified start of segment')
+            % legend('samples', 'samples with masked start/end', 'abs(diff(y))', 'max_adc_noise', 'final segment starts')
             title(sprintf('PJVS section with PJVS phase identification\nall data'))
             hold off
             fn = fullfile(dbg.plotpath, [ssec 'pjvs_ident_segments_all']);
@@ -143,6 +122,54 @@ function Spjvs = pjvs_ident_segments(y, MRs, MRe, max_adc_noise, segmentlen, dbg
     end % if debug
 
 end % function Spjvs = identify_pjvs_segments
+
+function Spjvs = highest_peak(y, segmentlen); %<<<1
+% Finds phase based on highest peak of derivation. Highest peak is most
+% hopefully a step change and not some noise or error. Method is fast.
+
+    % find step changes:
+    % derivation shows out voltage changes:
+    ydiff = abs(diff(y));
+    % get index of biggest derivation (biggest signal step);
+    [~, maxid] = max(ydiff);
+    maxid = maxid(1);
+    % remainder of division is offset - number of samples at beginning before
+    % first PJVS step happen:
+    phase_offset = rem(maxid, segmentlen);
+    % indexes of PJVS steps:
+    Spjvs = [1:segmentlen:numel(y)] + phase_offset;
+    % ensure integers:
+    Spjvs = round(Spjvs);
+    % ensure proper values of indexes:
+    Spjvs(Spjvs < 1) = [];
+    Spjvs(Spjvs > numel(y) + 1) = [];
+end % function Spjvs = highest_peak(y, segmentlen);
+
+function Spjvs = split_and_highest_peak(y, segmentlen); %<<<1
+    % Finds phase based on highest peak of derivation. Data are first cut to
+    % subsections of approximatly 50 pjvs periods (50*segmentlen).
+
+    % Pad y to be multiple of segmentlen for easy cutting. Last value of y is
+    % used to prevent derivations:
+    rsl = round(segmentlen).*50;
+    padlen = ceil(size(y,2)/rsl)*rsl - size(y,2);
+    ypad = [y y(end).*ones(1,padlen)];
+
+    Spjvs = [];
+    for j = 1:size(ypad,2)./rsl % this should be divisible already
+        tmp = highest_peak(ypad((j-1)*rsl + 1 : j*rsl), segmentlen);
+        Spjvs = [Spjvs (tmp + (j-1)*rsl)];
+    end % for j
+    % Sometimes can happen there is difference of 1 between 2 consecutive Spjvs
+    % indexes (like [... 340 350 351 361 ...]). This happens on the borders of two
+    % subsections, because of rounding of the phase (previous run was slightly below
+    % .5, next run slighly above .5). Therefore in previous run last point appeared,
+    % and in this run first point appeared also. Such points are removed:
+    idx = find(diff(Spjvs) == 1);
+    Spjvs(idx) = [];
+    % remove all Spjvs for index bigger than unpadded data:
+    Spjvs(Spjvs > size(y,2)) = [];
+end % function Spjvs = split_and_highest_peak(y, segmentlen);
 
 % tests  %<<<1
 %XXX not finished %assert
