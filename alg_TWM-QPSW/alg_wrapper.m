2e3d9c2c68e3c03ff86d2798c61f122ada6ced5a
KaeroDot <KaeroDot@contact.me.on.git.hub>
2022-09-21 14:14:09 +0200

    add: octave: correct QPS alg_info, alg_wrapper works up to demultiplex

--- alg_TWM-QPSW/alg_wrapper.m
+++ alg_TWM-QPSW/alg_wrapper.m
@@ -11,6 +11,11 @@ function dataout = alg_wrapper(datain, calcset)
 DEBUG = 0;
 
 % Prepare data --------------------------- %<<<1
+% convert N+1 dimensional matrices back to cell of structures
+diC = matrices_to_cells(datain);
+% Now is every phase in structure of cell of dic. The structure in cell is
+% ready to be sent into e.g. TWM-PWRFFT algorithm.
+
 % deal optional Rs, Re:
 if ~isfield(datain, 'Rs')
     datain.Rs.v = 0;
@@ -19,44 +24,46 @@ if ~isfield(datain, 'Re')
     datain.Rs.v = 0;
 end
 
-% input quantities:
-% general algorithm setting quantities:
-% 'support_multi_inputs'
-% local only for this algorithm:
-% Qlocal = {'alg', 'M', 'S', 'Uref', 'Spjvs', 'Rs', 'Re'}
-% TWM general:
-Qtwmg = {'fs', 'Ts', 't'};
-% TWM per phase:
-% (these quantities got suffix with number of channel, e.g. y -> y1, y2, y3, y4, y5, y6)
-% Qtwmpp = {'y', 'y_lo', 'time_shift_lo', 'adc_bits', 'adc_nrng', 'adc_lsb', 'adc_jitter', 'adc_aper_corr', 'adc_aper', 'adc_jitter', 'adc_offset', 'adc_gain_f', 'adc_gain_a', 'adc_gain', 'adc_phi_f', 'adc_phi_a', 'adc_phi', 'adc_sfdr_f', 'adc_sfdr_a', 'adc_sfdr', 'adc_Yin_f', 'adc_Yin_Cp', 'adc_Yin_Gp', 'tr_gain_f', 'tr_gain_a', 'tr_gain', 'tr_phi_f', 'tr_phi_a', 'tr_phi', 'tr_Zlo_f', 'tr_Zlo_Rp', 'tr_Zlo_Cp', 'tr_Zca_f', 'tr_Zca_Ls', 'tr_Zca_Rs', 'tr_Yca_f', 'tr_Yca_Cp', 'tr_Yca_D', 'Zcb_f', 'Zcb_Ls', 'Zcb_Rs', 'Ycb_f', 'Ycb_Cp', 'Ycb_D'};
+% LEFTOVER?:
+% % input quantities:
+% % general algorithm setting quantities:
+% % 'support_multi_inputs'
+% % local only for this algorithm:
+% % Qlocal = {'alg', 'M', 'S', 'Uref', 'Spjvs', 'Rs', 'Re'}
+% % TWM general:
+% Qtwmg = {'fs', 'Ts', 't'};
+% % TWM per phase:
+% % (these quantities got suffix with number of channel, e.g. y -> y1, y2, y3, y4, y5, y6)
+% % Qtwmpp = {'y', 'y_lo', 'time_shift_lo', 'adc_bits', 'adc_nrng', 'adc_lsb', 'adc_jitter', 'adc_aper_corr', 'adc_aper', 'adc_jitter', 'adc_offset', 'adc_gain_f', 'adc_gain_a', 'adc_gain', 'adc_phi_f', 'adc_phi_a', 'adc_phi', 'adc_sfdr_f', 'adc_sfdr_a', 'adc_sfdr', 'adc_Yin_f', 'adc_Yin_Cp', 'adc_Yin_Gp', 'tr_gain_f', 'tr_gain_a', 'tr_gain', 'tr_phi_f', 'tr_phi_a', 'tr_phi', 'tr_Zlo_f', 'tr_Zlo_Rp', 'tr_Zlo_Cp', 'tr_Zca_f', 'tr_Zca_Ls', 'tr_Zca_Rs', 'tr_Yca_f', 'tr_Yca_Cp', 'tr_Yca_D', 'Zcb_f', 'Zcb_Ls', 'Zcb_Rs', 'Ycb_f', 'Ycb_Cp', 'Ycb_D'};
+%
+% % convert standard TWM inputs into cells %<<<2
+% % find quantities for k and save them to datain
+% datainc = cell(1,6);
+% for k = 1:6
+%     numstr = sprintf('%d',k);
+%     Qnames = fieldnames(datain);
+%     numlen = numel(numstr);
+%     datainc{k} = struct();
+%     for l = 1:numel(Qnames)
+%         supposednumber = Qnames{l}(end-numlen+1 : end);
+%         if strcmpi(supposednumber, numstr)
+%             Qnamenew = Qnames{l}(1:end-numlen);
+%             datainc{k} = setfield(datainc{k}, Qnamenew, getfield(datain, Qnames{l}));
+%         elseif isnan(round(str2double(supposednumber)))
+%             datainc{k} = setfield(datainc{k}, Qnames{l}, getfield(datain, Qnames{l}));
+%         end
+%     end % for l = 1:numel(Qnames)
+% end % for k = 1:6
 
-% convert standard TWM inputs into cells %<<<2
-% find quantities for k and save them to datain
-datainc = cell(1,6);
-for k = 1:6
-    numstr = sprintf('%d',k);
-    Qnames = fieldnames(datain);
-    numlen = numel(numstr);
-    datainc{k} = struct();
-    for l = 1:numel(Qnames)
-        supposednumber = Qnames{l}(end-numlen+1 : end);
-        if strcmpi(supposednumber, numstr)
-            Qnamenew = Qnames{l}(1:end-numlen);
-            datainc{k} = setfield(datainc{k}, Qnamenew, getfield(datain, Qnames{l}));
-        elseif isnan(round(str2double(supposednumber)))
-            datainc{k} = setfield(datainc{k}, Qnames{l}, getfield(datain, Qnames{l}));
-        end
-    end % for l = 1:numel(Qnames)
-end % for k = 1:6
-
-% convert sampled signals into matrix %<<<2
-for k = 1:numel(datainc)
-    ch(k, :) = datainc{k}.y.v(:)';
+% convert sampled signals into matrix: %<<<2
+for k = 1:numel(diC)
+    ch(2*k - 1, :) = diC{k}.u.v(:)';
+    ch(2*k,     :) = diC{k}.i.v(:)';
 end % for k
 
 % qpsw process --------------------------- %<<<1
 % reordering of the data and calibration %<<<2
-[yc, S, I] = qpsw_demultiplex_split(ch, datain.S, datain.M);
+[yc, S, I] = qpsw_demultiplex_split(ch, diC{1}.S.v, diC{1}.M.v);
     % THIS DOES NOT GENERATE TIME STAMPS PROPERLY! XXXXXXXXXXXXX
     % we need to split datainc in proper cells, not only y as in
     % qpsw_demultiplex, but also timestamps, and assign them to actual datainc 
@@ -121,6 +128,7 @@ if DEBUG
 end % if DEBUG
 
 % calculate amplitude and phases of data pieces %<<<2
+% Crude method, remove later: XXX
 res = struct();
 for i = 1:rows(yc)
         if My(i) > 0 % only non-quantum data
@@ -134,7 +142,6 @@ for i = 1:rows(yc)
             end % for j = 1:columns(yc)
         end % if My > 0 % only non-quantum data
 end % for i = 1:rows(yc)
-
 
 % apply algorithm for actual data pieces %<<<2
 
@@ -153,5 +160,94 @@ keyboard
 % --- outputs %<<<1
 dataout.DO.v = 5;
 dataout.DXXX.v = 8;
+
+end % function dataout = alg_wrapper(datain, calcset)
+
+function [diC] = matrices_to_cells(din, alginfo) %<<<1
+% Reorder data from the Cell diC of size N with structures with matrices to a
+% Strucure with matrices of one dimension more. The added dimension will have
+% size N.
+% This is done with respect to the qwtb and TWM quantities, so parameter
+% dimensions are not increased.
+% If matrices diC{i}.Q.F and diC{j}.Q.F are not of same dimensions, they are
+% padded by NaNs to the size of larger one.
+% Structures in cells must have the same fields to whole depth.
+
+
+% Reorder data from a structure with matrices of dimension N+1 into cells of structures
+% with matrices of dimension N. The last dimension was used for different phases of the sampled system.
+% This is done with respect to the qwtb and TWM quantities, so parameter
+% dimensions are not increased.
+% Script qwtb_exec_algorithm.m did the conversion from cell of structs to
+% struct of N+1 matrices. If matrices were of incompatible sizes, padding by
+% NaNs occured.
+% This function removes the padding back.
+
+    % List of possible quantity fields in QWTB:
+    QWTBf = {'v', 'u', 'd', 'c', 'r'};
+
+    % list of quantities of the algorithm:
+    Qs = fieldnames(din);
+
+    % Go through fields 'v' of all quantities and find size of the latest
+    % dimension:
+    max_dim = [];
+    for q = 1:numel(Qs)
+        Q = Qs{q};
+        % only if not parameter:
+        % (every quantity gets field 'par', that is added by QWTB)
+        if not(din.(Q).par)
+            % take size of last dimension:
+            max_dim(end+1) = size(din.(Q).v)(end);
+        end % if not(Q.par)
+    end
+    if not(all(max_dim(1) == max_dim))
+        error('QPSW wrapper - last dimension of some quantity is not same as of others. Concatenating all phases into one did not worked properly.')
+    end
+    max_dim = max(max_dim);
+
+    % Prepare output cell for every phase:
+    diC = cell(max_dim, 1);
+
+    % For all quantities in the structure, for all fields, split matrices by last
+    % dimension and put them into cells of structures.
+    % From din.Q_j.F_k of size (m x n x i) -> diC{i}.Q_j.F_k of sizes (m x n)
+    for q = 1:numel(Qs)
+        Q = Qs{q};
+        if din.(Q).par
+        % If quantity is parameter type, only take value from first cell:
+        % (every quantity gets field 'par', that is added by qwtb)
+            for f = 1:numel(QWTBf)
+                F = QWTBf{f};
+                if isfield(din.(Q), F);
+                    for c = 1:max_dim
+                        diC{c}.(Q).(F) = din.(Q).(F);
+                    end
+                end % if isfield(din.(Q), F);
+            end
+        else
+            % Q is not of parameter type, split matrix into cells:
+            for f = 1:numel(QWTBf)
+                F = QWTBf{f};
+                % check existence of field! XXX
+                if isempty(din.(Q).(F))
+                    for c = 1:max_dim
+                        diC{c}.(Q).(F) = [];
+                    end
+                else
+                    % use subscript reference assignement because number of
+                    % dimensions of the matrices is not known:
+                    mat = din.(Q).(F);
+                    S.subs = repmat({':'}, 1, ndims(mat));
+                    S.type = '()';
+                    for c = 1:max_dim
+                        S.subs{end} = c;
+                        diC{c}.(Q).(F) = subsref(mat, S);
+                    end
+                end % if isempty
+            end % for f
+        end % if din.(Q).par
+    end % for q
+end % function [diC] = matrices_to_cells(din, alginfo)
 
 % vim settings modeline: vim: foldmarker=%<<<,%>>> fdm=marker fen ft=octave textwidth=80 tabstop=4 shiftwidth=4
